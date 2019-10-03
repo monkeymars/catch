6748c91d9ef6a2886a6bc26ff63d35d082ffbc90
Gery Wibowo <akun.gery@gmail.com>
2019-10-04 00:55:46 +0700

    Shopping Cart Component

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Header, Grid, Icon } from 'semantic-ui-react'
 
 const ShoppingCartComponent = () => {
   return (
     <>
-      <Header as='h3' disabled>Shopping Cart</Header>
+      <Grid container>
+        <Grid.Column width={16}>
+          <Header as='h3' disabled>Shopping Cart</Header>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          [1]
+        </Grid.Column>
+        <Grid.Column width={9}>
+          [Product Name]
+          <div style={{marginTop: 8}}>
+            <span style={{marginLeft: 8}}><Icon name="plus circle"/></span>
+            <span style={{marginLeft: 8}}><Icon name="minus circle"/></span>
+            <span style={{marginLeft: 8}}><Icon name="remove circle"/></span>
+          </div>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          [price]
+        </Grid.Column>
+      </Grid>
     </>
   )
 }

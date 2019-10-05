57de6cec4f6fb9ba2a47323d924ecbf0e71316e0
Gery Wibowo <akun.gery@gmail.com>
2019-10-05 13:56:12 +0700

    http services

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: process.env.BASE_URL
+  baseURL: process.env.REACT_APP_BASE_URL
 })
 
 export const get = async (endpoint, options = {}) => {

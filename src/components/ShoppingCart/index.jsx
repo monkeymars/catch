47c01e837b4a27a548023cdc5c1a954554f93a4d
Gery Wibowo <akun.gery@gmail.com>
2019-10-05 19:20:32 +0700

    show total cart

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import { Header, Grid, Icon } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import currencyFormatter from 'currency-formatter'
+import { addQty, removeQty, removeItem } from '../../redux/actions/cart.action'
 
-const ShoppingCartComponent = ({cart}) => {
-  console.log(cart)
+const ShoppingCartComponent = ({
+  cart,
+  handleAddQty,
+  handleRemoveQty,
+  handleRemoveItem
+}) => {
+  const getTotal = () => {
+    let total = 0
+    cart.listProduct.forEach(product => {
+      console.log(product)
+      total = total + (product.salePrice * product.qty)
+    });
+    return total
+  }
   return (
     <>
       <Grid container>
@@ -13,6 +26,9 @@ const ShoppingCartComponent = ({cart}) => {
         </Grid.Column>
       </Grid>
       <Grid container>
+        {!cart.listProduct.length && (
+          <Header as='h6'>You have no items in your cart.</Header>
+        )}
         {cart.listProduct.map(product => (
           <Grid container key={product.id}>
             <Grid.Column width={3}>
@@ -21,9 +37,25 @@ const ShoppingCartComponent = ({cart}) => {
             <Grid.Column width={9}>
               {product.name}
               <div className="btn-group__cart-actions m_t_sm">
-                <span><Icon name="plus circle"/></span>
-                <span><Icon name="minus circle"/></span>
-                <span><Icon name="remove circle"/></span>
+                <span>
+                  <Icon onClick={()=>{
+                    handleAddQty(product)
+                  }} name="plus circle"/>
+                </span>
+                <span>
+                  <Icon onClick={()=>{
+                    if (product.qty > 1) {
+                      handleRemoveQty(product)
+                    } else {
+                      handleRemoveItem(product)
+                    }
+                  }} name="minus circle"/>
+                </span>
+                <span>
+                  <Icon onClick={()=>{
+                    handleRemoveItem(product)
+                  }} name="remove circle"/>
+                </span>
               </div>
             </Grid.Column>
             <Grid.Column width={4}>
@@ -32,6 +64,16 @@ const ShoppingCartComponent = ({cart}) => {
           </Grid>
         ))}
       </Grid>
+      {cart.listProduct.length >= 1 && (
+        <Grid container>
+          <Grid.Column width={8}>
+            <strong>Total</strong>
+          </Grid.Column>
+          <Grid.Column style={{textAlign: 'right'}} width={8}>
+            <strong>{currencyFormatter.format(getTotal(), { code: 'USD' })}</strong>
+          </Grid.Column>
+        </Grid>
+      )}
     </>
   )
 }
@@ -44,11 +86,14 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addQty: () => {
-      console.log('addQty')
+    handleAddQty: (product) => {
+      dispatch(addQty(product))
     },
-    removeQty: () => {
-      console.log('addQty')
+    handleRemoveQty: (product) => {
+      dispatch(removeQty(product))
+    },
+    handleRemoveItem: (product) => {
+      dispatch(removeItem(product))
     }
   }
 }

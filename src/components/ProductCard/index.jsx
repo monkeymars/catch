d25360c6e9bad08b89c51724375d0e92b6b7e172
Gery Wibowo <akun.gery@gmail.com>
2019-10-05 17:54:52 +0700

    add to cart on catalog component; render cart on shppping cart component

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,40 +1,70 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { addCart } from '../../redux/actions/cart.action'
 import { Header, Card, Image, Button } from 'semantic-ui-react'
+import currencyFormatter from 'currency-formatter'
 
-const ProductCardComponent = ({productDetail}) => {
+const ProductCardComponent = ({productDetail, addProduct}) => {
   return (
     <Card>
+      {/* Product Image */}
       <Image src={productDetail.imageUrl} wrapped ui={false} />
+
+      {/* Add to Cart */}
       {productDetail.quantityAvailable ? (
-        <Button color='teal' size='mini'>Add to Cart</Button>
+        <Button onClick={() => addProduct(productDetail)}
+          color='teal' size='mini'>
+          Add to Cart
+        </Button>
       ):(
         <Button disabled size='mini'>Sold Out</Button>
       )}
 
+      {/* Product Name */}
       <Card.Content>
         <Card.Header>
           <Header as='h6'>{productDetail.name}</Header>
         </Card.Header>
       </Card.Content>
+
+      {/* Price */}
       <Card.Content extra>
         <div style={{textAlign: 'center'}}>
           { productDetail.retailPrice !== 0 && (
             <span id="retail-price" style={{textDecoration: 'line-through'}}>
-              {productDetail.retailPrice}
+              {currencyFormatter.format(productDetail.retailPrice, { code: 'USD' })}
             </span>
           )}
         </div>
         <div style={{textAlign: 'center'}}>
-          <span style={{fontSize: 18}} id="sales-price">{productDetail.salePrice}</span>
+          <span style={{fontSize: 18}} id="sales-price">
+            {currencyFormatter.format(productDetail.salePrice, { code: 'USD' })}
+          </span>
         </div>
       </Card.Content>
     </Card>
   )
 }
 
+const mapStateToProps = state => {
+  return { cart: state.cart }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addProduct: product => {
+      product.qty = 1
+      dispatch(addCart(product))
+    }
+  }
+}
+
 ProductCardComponent.propTypes = {
   data: PropTypes.object
 }
 
-export default ProductCardComponent
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductCardComponent)
--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -1,30 +1,56 @@
 import React from 'react'
 import { Header, Grid, Icon } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import currencyFormatter from 'currency-formatter'
 
-const ShoppingCartComponent = () => {
+const ShoppingCartComponent = ({cart}) => {
+  console.log(cart)
   return (
     <>
       <Grid container>
         <Grid.Column width={16}>
           <Header as='h3' disabled>Shopping Cart</Header>
         </Grid.Column>
-        <Grid.Column width={3}>
-          [1]
-        </Grid.Column>
-        <Grid.Column width={9}>
-          [Product Name]
-          <div style={{marginTop: 8}}>
-            <span style={{marginLeft: 8}}><Icon name="plus circle"/></span>
-            <span style={{marginLeft: 8}}><Icon name="minus circle"/></span>
-            <span style={{marginLeft: 8}}><Icon name="remove circle"/></span>
-          </div>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          [price]
-        </Grid.Column>
+      </Grid>
+      <Grid container>
+        {cart.listProduct.map(product => (
+          <Grid container key={product.id}>
+            <Grid.Column width={3}>
+              {`${product.qty}x`}
+            </Grid.Column>
+            <Grid.Column width={9}>
+              {product.name}
+              <div className="btn-group__cart-actions m_t_sm">
+                <span><Icon name="plus circle"/></span>
+                <span><Icon name="minus circle"/></span>
+                <span><Icon name="remove circle"/></span>
+              </div>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              {currencyFormatter.format(product.salePrice, { code: 'USD' })}
+            </Grid.Column>
+          </Grid>
+        ))}
       </Grid>
     </>
   )
 }
 
-export default ShoppingCartComponent
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addQty: () => {
+      console.log('addQty')
+    },
+    removeQty: () => {
+      console.log('addQty')
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartComponent)
